66d3764a96da5a6eb336ba2df34d88a30584c350
emmanowo04 <emmanuel_owolabi@hotmail.com>
2026-09-07 11:19:45 +0100

    Add files via upload

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://lulgniwfbejahkfhmazb.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1bGduaXdmYmVqYWhrZmhtYXpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2MjQwNjAsImV4cCI6MjA5MDIwMDA2MH0.L6IpWtDmEJ-MXOp_bMrawUyfEFwXuD5TT_fA2JGkDrc';
+const supabaseUrl = 'https://qolirpmnucvcfkhuwshj.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvbGlycG1udWN2Y2ZraHV3c2hqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg3NzA4ODcsImV4cCI6MjEwNDM0Njg4N30.IAWsoKZAHRu2JkuDz2OdHcHkIF3dsWiAOb_84ggQ3Es';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
